JuliaQuantumControl/QuantumOpticsControlExtensions.jl

Rename JuliaQuantumControl org

docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(QuantumOpticsControlExtensions, :DocTestSetup, :(using Quant
 makedocs(;
     modules=[QuantumOpticsControlExtensions],
     authors="Michael Goerz <mail@michaelgoerz.net> and contributors",
-    repo="https://github.com/QuantumControl-jl/QuantumOpticsControlExtensions.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/JuliaQuantumControl/QuantumOpticsControlExtensions.jl/blob/{commit}{path}#{line}",
     sitename="QuantumOpticsControlExtensions.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://QuantumControl-jl.github.io/QuantumOpticsControlExtensions.jl",
+        canonical="https://juliaquantumcontrol.github.io/QuantumOpticsControlExtensions.jl",
         assets=String[],
     ),
     pages=[
@@ -19,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/QuantumControl-jl/QuantumOpticsControlExtensions.jl",
+    repo="github.com/JuliaQuantumControl/QuantumOpticsControlExtensions.jl",
 )

JuliaQuantumControl/QuantumPropagators.jl

docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(QuantumPropagators, :DocTestSetup, :(using QuantumPropagator
 makedocs(;
     modules=[QuantumPropagators],
     authors="Michael Goerz <mail@michaelgoerz.net> and contributors",
-    repo="https://github.com/QuantumControl-jl/QuantumPropagators.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/JuliaQuantumControl/QuantumPropagators.jl/blob/{commit}{path}#{line}",
     sitename="QuantumPropagators.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://quantumcontrol-jl.github.io/QuantumPropagators.jl",
+        canonical="https://juliaquantumcontrol.github.io/QuantumPropagators.jl",
         assets=String[],
     ),
     pages=[
@@ -29,5 +29,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/QuantumControl-jl/QuantumPropagators.jl",
+    repo="github.com/JuliaQuantumControl/QuantumPropagators.jl",
 )