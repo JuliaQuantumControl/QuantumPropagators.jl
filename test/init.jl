13e77c110df1f48f89df44f0ce1a5ec9a2dc688e
Michael Goerz <goerz@stanford.edu>
2022-02-14 11:45:08 -0500

    Add help() command in dev-repl

--- test/init.jl
+++ test/init.jl
@@ -8,12 +8,13 @@ using LiveServer: LiveServer, serve, servedocs
 include(joinpath(@__DIR__, "clean.jl"))
 
 
-println("""
+REPL_MESSAGE = """
 *******************************************************************************
 DEVELOPMENT REPL
 
 Revise, JuliaFormatter, LiveServer, Plots with unicode backend are active.
 
+* `help()` – Show this message
 * `include("test/runtests.jl")` – Run the entire test suite
 * `test()` – Run the entire test suite in a subprocess with coverage
 * `test(genhtml=true)` – Generate an HTML coverage report
@@ -27,4 +28,9 @@ Revise, JuliaFormatter, LiveServer, Plots with unicode backend are active.
 * `clean()` – Clean up build/doc/testing artifacts
 * `distclean()` – Restore to a clean checkout state
 *******************************************************************************
-""")
+"""
+
+"""Show help"""
+help() = println(REPL_MESSAGE)
+
+help()
