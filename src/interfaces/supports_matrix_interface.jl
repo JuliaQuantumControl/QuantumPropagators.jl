b1c80ccdc33aedfc49db24469e52f8f00e8af77b
Michael Goerz <mail@michaelgoerz.net>
2026-02-18 11:04:26 -0500

    Declare `supports_matrix_interface` for operators

--- src/interfaces/supports_matrix_interface.jl
+++ src/interfaces/supports_matrix_interface.jl
@@ -13,9 +13,6 @@ of `AbstractMatrix`. Calling `supports_matrix_interface` on an instance
 `x` also works via a convenience fallback that forwards to
 `supports_matrix_interface(typeof(x))`.
 
-Depending the value of [`supports_inplace`](@ref), `T` may implement a
-read-write matrix (`setindex!` etc.) or a read-only matrix.
-
 The matrix interface is encouraged for [operators](@ref Operators), and the
 specific conditions of the required interface in that context are checked via
 [`QuantumPropagators.Interfaces.check_operator`](@ref).
@@ -25,3 +22,10 @@ supports_matrix_interface(::Type{<:AbstractMatrix}) = true
 supports_matrix_interface(::Type{T}) where {T} = false
 
 supports_matrix_interface(x) = supports_matrix_interface(typeof(x))
+
+import ..Operator
+import ..ScaledOperator
+
+supports_matrix_interface(::Type{<:Operator{OT}}) where {OT} = supports_matrix_interface(OT)
+supports_matrix_interface(::Type{<:ScaledOperator{<:Any,OT}}) where {OT} =
+    supports_matrix_interface(OT)
